e9fdf71f53d194a6ca12b37a2b53ff9c02f3e0fb
thanhitkeaung09 <thanhtikeaungupwork@gmail.com>
2023-11-14 12:46:23 +0630

    error  fixing

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Search from '@/app/ui/search';
 import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
-
+// import { lusitana } from '@/app/ui/fonts';
 export default async function CustomersTable({
   customers,
 }: {
